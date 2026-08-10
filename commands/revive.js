46a0cf84ce179fb5924a150a1274e23b7a0ea4d9
Agro388 <agrobaticfinger388@gmail.com>
2026-08-10 06:55:37 +0200

    Enhance revive command with self-revive option
    Add self-revive functionality and update revive command

--- commands/revive.js
+++ commands/revive.js
@@ -8,11 +8,22 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('revive')
         .setDescription('Bring someone back to life!')
-        .setIntegrationTypes([0, 1]) // Guild & User App
+        .setIntegrationTypes([0, 1])
         .setContexts([0, 1, 2])
         .addUserOption(option => option.setName('target').setDescription('Who are you reviving?').setRequired(true)),
 
     async execute(interaction, senderName, recipientName) {
+        const targetUser = interaction.options.getUser('target');
+
+        if (interaction.user.id === targetUser.id) {
+            const selfVariants = [
+                `✨ ${senderName} used a Revive Potion on themselves...`,
+                `⚡ ${senderName} shocked themselves with the defibrillator!`,
+                `🟩 ${senderName} self-revived!`
+            ];
+            return getRandomMessage(selfVariants);
+        }
+
         const reviveVariants = [
             `✨ ${senderName} cast a resurrection spell on ${recipientName}! Welcome back to life!`,
             `⚡ ${senderName} brought out the defibrillator and shocked ${recipientName} back into consciousness! *CLEAR!*`,
@@ -20,7 +31,6 @@ module.exports = {
             `🕊️ ${senderName} pulled ${recipientName} back from the afterlife just in time!`,
             `🟩 ${senderName} held down the revive button on ${recipientName}! Back on their feet!`
         ];
-
         return getRandomMessage(reviveVariants);
     }
 };
