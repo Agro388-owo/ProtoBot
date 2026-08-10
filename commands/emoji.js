6ef3d057fed6ec606f2bbda436c8690dfdd645da
Agro388 <agrobaticfinger388@gmail.com>
2026-08-10 19:52:40 +0200

    Add emoji command to send custom bot emojis
    This command allows users to send custom emojis by name.

---
+++ commands/emoji.js
@@ -0,0 +1,49 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+// Map of available custom emoji names to their full Discord string format
+const emojiMap = {
+    'maiddress': '<:maiddress:1536430032572911646>',
+    'protogenirl': '<:protogenirl:1536430038751121499>',
+    'protoram': '<:protoram:1536430036524204113>',
+    'protogenpop11': '<:protogenpop11:1536430034561269780>',
+    'Purodance': '<:Purodance:1536429613641760768>',
+    'puro_spin': '<:puro_spin:1536429933562175648>',
+    'Puropreocupado': '<:Puropreocupado:1536430030916288572>',
+    'Puro_Blush6': '<:Puro_Blush6:1536430029104353380>',
+    'Puro_Pathetic6': '<:Puro_Pathetic6:1536430027468710019>',
+    'puro_sad': '<:puro_sad:1536430025635799061>',
+    'purocute': '<:purocute:1536367584369180803>',
+    'puronervous': '<:puronervous:1536367581995335750>',
+    'puroshock': '<:puroshock:1536366927230799972>',
+    'puroblush': '<:puroblush:1536364136613806090>',
+    'puroneutral': '<:puroneutral:1536364135342669824>',
+    'Puroadorable': '<:Puroadorable:1536364133392457818>'
+};
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('emoji')
+        .setDescription('Send one of your custom bot emojis!')
+        .setIntegrationTypes([0, 1])
+        .setContexts([0, 1, 2])
+        .addStringOption(option =>
+            option.setName('name')
+                  .setDescription('The name of the emoji')
+                  .setRequired(true)
+                  .addChoices(
+                      ...Object.keys(emojiMap).slice(0, 25).map(name => ({ name, value: name }))
+                  )
+        ),
+
+    async execute(interaction) {
+        const emojiName = interaction.options.getString('name');
+        const emojiString = emojiMap[emojiName];
+
+        if (!emojiString) {
+            return interaction.reply({ content: 'Emoji not found!', ephemeral: true });
+        }
+
+        return emojiString;
+    }
+};
+
