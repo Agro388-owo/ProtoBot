6a5b06c81fdd4dae41e6a3a0face3d9ca18c4078
Agro388 <agrobaticfinger388@gmail.com>
2026-08-10 06:28:43 +0200

    Add initial configuration for Discord bot

---
+++ config.js
@@ -0,0 +1,14 @@
+// config.js (Root folder)
+module.exports = {
+    // Put your Discord User ID here
+    OWNER_ID: '1048579285687996466',
+
+    // Toggle state (default: true = restrictions enabled)
+    kidnapRestricted: true,
+
+    // List of allowed user IDs (If empty, everyone can use it when enabled)
+    allowedUsers: [
+        // '1230079549123858507',
+        // '1048579285687996466'
+    ]
+};
