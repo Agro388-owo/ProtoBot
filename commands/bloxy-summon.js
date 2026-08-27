493ab89a41284d4067dbeaad4d7d45a54342c3a6
Agro388 <agrobaticfinger388@gmail.com>
2026-08-27 08:32:24 +0200

    Create bloxy-summon.js

---
+++ commands/bloxy-summon.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
+const path = require('path');
+const fs = require('fs');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('bloxy')
+        .setDescription('Summon Bloxy.'),
+
+    async execute(interaction) {
+        const filePath = path.join(__dirname, '..', 'assets/goober/Bloxy.png');
+
+        if (!fs.existsSync(filePath)) {
+            return interaction.reply({ content: `⚠️ Bloxy.png not found at assets/goober/!`, ephemeral: true });
+        }
+
+        const file = new AttachmentBuilder(filePath);
+
+        await interaction.reply({ 
+            files: [file] 
+        });
+    }
+};
